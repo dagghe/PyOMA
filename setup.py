96ff3a4d90a21afe5b69496052583f809c0ea273
lollix91 <lollix@live.it>
2021-05-05 01:46:09 +0200

    a

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'PyOMA',
+    packages = ['source'],
+    version = 'v1.1',  # Ideally should be same as your GitHub release tag varsion
+    description = 'OMA allows the experimental estimation of the modal parameters (natural frequencies, mode shapes, damping ratios) of a structure from measurements of the vibration response in operational condition.',
+    author = 'Dag Pasquale Pasca, Angelo Aloisio, Lorenzo De Lauretis',
+    author_email = '',
+    url = 'https://github.com/dagghe/PyOMA/tree/master/source',
+    download_url = 'https://github.com/dagghe/PyOMA/archive/refs/tags/v1.1.tar.gz',
+    keywords = ['OMA', 'structure'],
+    classifiers = [],
+)
